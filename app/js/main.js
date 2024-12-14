b9142ae5fa46c71ccc0453bde045803252bbdbb0
Christina <150205323+berryalien@users.noreply.github.com>
2024-12-14 18:17:38 +0700

    Added markup and styles for the feedback section.

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function () {
-  
-});
+
+  $('.article-slider__box').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/icons/icon-chevron-left.svg" alt="arrow left"></button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/icons/icon-chevron-right.svg" alt="arrow right"></button>'
+  });
+
+
+})
 
 
 var mixer = mixitup('.assortment__gallery');

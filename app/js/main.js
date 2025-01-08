6bda57052ec0e0eb3ecc63be509d3695a0cb3564
Christina <150205323+berryalien@users.noreply.github.com>
2025-01-08 19:36:28 +0700

    added pictures to the project

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,15 @@ $(function () {
     nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/icons/icon-chevron-right.svg" alt="arrow right"></button>'
   });
 
+  var header = document.getElementById("assortment__filter");
+  var btns = header.getElementsByClassName("assortment__btn-food");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
 
 })
 

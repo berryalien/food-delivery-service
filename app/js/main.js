1401333bb8f41a420243f9109469874f3481ce4c
Christina <150205323+berryalien@users.noreply.github.com>
2025-03-06 20:08:07 +0700

    added changes for the index page. Fixed section names, class names, changed markup structure.

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,21 @@
 $(function () {
 
-  $('.article-slider__box').slick({
-    dots: true,
-    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/icons/icon-chevron-left.svg" alt="arrow left"></button>',
-    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/icons/icon-chevron-right.svg" alt="arrow right"></button>'
-  });
-
-  var header = document.getElementById("assortment__filter");
-  var btns = header.getElementsByClassName("assortment__btn-food");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
-  }
-
-})
-
 
 var mixer = mixitup('.assortment__gallery');
+
+const swiper = new Swiper('.swiper', {
+  loop: false,
+
+  pagination: {
+    el: '.swiper-pagin',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-btn--prev',
+    prevEl: '.swiper-btn--next',
+  },
+});
+});
+
+

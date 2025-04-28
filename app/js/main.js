d2c8ccd2e7d2edc6db40b8152640632bba608f64
Christina <150205323+berryalien@users.noreply.github.com>
2025-04-29 00:15:52 +0700

    project is finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,215 @@
 $(function () {
 
+  $('.select-styler, .product__input').styler();
 
-var mixer = mixitup('.assortment__gallery');
 
-const swiper = new Swiper('.swiper', {
-  loop: false,
+  $('.burger').on('click', function () {
+    $('.burger').toggleClass('burger__line--active');
 
-  pagination: {
-    el: '.swiper-pagin',
-    clickable: true,
-  },
+    $('body').toggleClass('lock');
+    $('.burger-nav').toggleClass('burger-nav--active');
+  });
 
-  navigation: {
-    nextEl: '.swiper-btn--prev',
-    prevEl: '.swiper-btn--next',
-  },
+  $('.burger-filter').on('click', function () {
+    $('.burger-filter').toggleClass('burger-filter--active');
+    $('.catalog__filter-box').toggleClass('catalog__filter-box--active');
+    $('body').toggleClass('lock');
+  });
+
+  $('.description__link').on('click', function (e) {
+    e.preventDefault();
+    $('.description__link').removeClass('description__link--active');
+    $(this).addClass('description__link--active');
+
+    $('.description__content').removeClass('description__content--active');
+    $($(this).attr('href')).addClass('description__content--active');
+  })
+
+  $('.product__img-link').on('click', function (tabs) {
+    tabs.preventDefault();
+    $('body').addClass('lock');
+
+    $('.product__console-wrapper').addClass('product__console-wrapper--active');
+  })
+
+  $('.product__console-btn').on('click', function () {
+    $('body').removeClass('lock');
+    $('.product__console-wrapper').removeClass('product__console-wrapper--active');
+  })
+
+  $('.nav__link').on('click',function () { 
+    $('.nav__link').removeClass('nav__link--active');
+    $(this).addClass('nav__link--active');
+  })
+
+  $(document).mouseup(function (e) {
+    var div = $('.burger');
+    if (!$('.product__console-wrapper, .burger-nav, .catalog__filter-box').is(e.target) &&
+      $('.product__console-wrapper, .burger-nav, .catalog__filter-box').has(e.target).length === 0) {
+      $('.burger').removeClass('burger__line--active');
+      $('.burger-filter').removeClass('burger-filter--active');
+      $('.burger-nav').removeClass('burger-nav--active');
+      $('.catalog__filter-box').removeClass('catalog__filter-box--active');
+      $('.product__console-wrapper').removeClass('product__console-wrapper--active');
+      $('body').removeClass('lock');
+    }
+  });
+
+  $('.filter-category__btn').on('click', function () {
+    $('.burger-filter').removeClass('burger-filter--active');
+    $('.catalog__filter-box').removeClass('catalog__filter-box--active');
+    $('body').removeClass('lock');
+  });
+
+  $(window).on('load resize', function () {
+    if ($(window).width() < 769) {
+      $('.restaurants__list:not(.slick-initialized), .discount__list:not(.slick-initialized)').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 12000,
+      });
+    }
+    if ($(window).width() > 769) {
+      $(".restaurants__list.slick-initialized").slick("unslick");
+      $(".discount__list.slick-initialized").slick("unslick");
+    }
+  });
+
+  $('.comments__slider:not(.slick-initialized)').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    infinite: false,
+    prevArrow: '<button type="button" class="comments__arrow comments__arrow--prev"><span class="sr-only">Предидущий слайд</span><svg class="icons-sprite"><use xlink:href="images/sprite.svg#icon-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="comments__arrow comments__arrow--next"><span class="sr-only">Следующий слайд</span><svg class="icons-sprite"><use xlink:href="images/sprite.svg#icon-right"></use></svg></button>',
+    appendArrows: '.comments__slider',
+    responsive: [{
+      breakpoint: 769,
+      settings: {
+        dots: false
+      }
+    }]
+  });
+
+  $('.product__images:not(.slick-initialized)').slick({
+    fade: true,
+    prevArrow: '<button type="button" class="product__arrow product__arrow--prev"><span class="sr-only">Предидущий слайд</span><svg class="product__icons-sprite"><use xlink:href="images/sprite.svg#icon-chevron-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="product__arrow product__arrow--next"><span class="sr-only">Следующий слайд</span><svg class="product__icons-sprite"><use xlink:href="images/sprite.svg#icon-chevron-left"></use></svg></button>',
+    appendArrows: '.product__images',
+    asNavFor: '.product__console',
+    responsive: [{
+      breakpoint: 769,
+      settings: {
+        dots: false,
+        arrows: false
+      }
+    }]
+  });
+
+  $('.product__console:not(.slick-initialized)').slick({
+    fade: true,
+    dots: true,
+    prevArrow: '<button type="button" class="product__arrow product__arrow--prev"><span class="sr-only">Предидущий слайд</span><svg class="product__icons-sprite"><use xlink:href="images/sprite.svg#icon-chevron-left"></use></svg></button>',
+    prevArrow: '<button type="button" class="product__arrow product__arrow--prev"><span class="sr-only">Предидущий слайд</span><svg class="product__icons-sprite"><use xlink:href="images/sprite.svg#icon-chevron-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="product__arrow product__arrow--next"><span class="sr-only">Следующий слайд</span><svg class="product__icons-sprite"><use xlink:href="images/sprite.svg#icon-chevron-left"></use></svg></button>',
+    appendArrows: '.product__console',
+    asNavFor: '.product__images',
+    responsive: [{
+      breakpoint: 769,
+      settings: {
+        dots: false
+      }
+    }]
+  });
+
+  $(".product__star").rateYo({
+    starWidth: "16px",
+    fullStar: true,
+    spacing: "6px",
+    normalFill: "#C1C1C1",
+    readOnly: true,
+    ratedFill: "#FFB800"
+  });
+
+  $(".description__stars").rateYo({
+    starWidth: "16px",
+    fullStar: true,
+    spacing: "6px",
+    normalFill: "#C1C1C1",
+    readOnly: true,
+    ratedFill: "#FFB800"
+  });
+
+  $(".description__form-stars").rateYo({
+    starWidth: "16px",
+    fullStar: true,
+    spacing: "6px",
+    normalFill: "#C1C1C1",
+    ratedFill: "#FFB800",
+  });
 });
+
+var $range = $(".filter-price__input");
+var $inputFrom = $(".filter-price__from");
+var $inputTo = $(".filter-price__to");
+var instance;
+var min = 0;
+var max = $inputTo;
+var from = 0;
+var to = 0;
+
+$range.ionRangeSlider({
+  type: "double",
+  onStart: updateInputs,
+  onChange: updateInputs,
+  onFinish: updateInputs
+});
+instance = $range.data("ionRangeSlider");
+
+function updateInputs(data) {
+  from = data.from;
+  to = data.to;
+
+  $inputFrom.prop("value", from);
+  $inputTo.prop("value", to);
+}
+
+$inputFrom.on("change", function () {
+  var val = $(this).prop("value");
+
+  // validate
+  if (val < min) {
+    val = min;
+  } else if (val > to) {
+    val = to;
+  }
+
+  instance.update({
+    from: val
+  });
+
+  $(this).prop("value", val);
+
 });
 
+$inputTo.on("change", function () {
+  var val = $(this).prop("value");
 
+  // validate
+  if (val < from) {
+    val = from;
+  } else if (val > max) {
+    val = max;
+  }
+
+  instance.update({
+    to: val
+  });
+
+  $(this).prop("value", val);
+});
+
+var mixer = mixitup('.choice__foods, .catalog__list');
+mixer.filter('.category-a');
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 const { src, dest, watch, parallel, series} = require('gulp');
-
 const scss          = require('gulp-sass')(require('sass'));
 const concat        = require('gulp-concat');
 const autoprefixer  = require('gulp-autoprefixer');
 const uglify        = require('gulp-uglify');
 const imagemin      = require('gulp-imagemin');
+// const del           = require('del');
 const browserSync   = require('browser-sync').create();
-const del           = require('del');
 const svgSprite     = require('gulp-svg-sprite');
 const cheerio       = require('gulp-cheerio');
 const replace       = require('gulp-replace');
+const fileInclude   = require('gulp-file-include');
 
 function browsersync() {
   browserSync.init({
@@ -22,7 +22,7 @@ function browsersync() {
 
 function styles() {
   return src('app/scss/style.scss')
-      .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(scss({outputStyle: 'compressed'}))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
@@ -34,9 +34,12 @@ function styles() {
 
 function scripts(){
   return src([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/mixitup/dist/mixitup.min.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
@@ -45,45 +48,12 @@ function scripts(){
   .pipe(browserSync.stream())
 }
 
-function svgSprites() {
-  return src('app/images/icons/*.svg') // выбираем в папке с иконками все файлы с расширением svg
-  .pipe(cheerio({
-    run: ($) => {
-      $("[fill]").removeAttr("fill"); // очищаем цвет у иконок по умолчанию, чтобы можно было задать свой
-      $("[stroke]").removeAttr("stroke"); // очищаем, если есть лишние атрибуты строк
-      $("[style]").removeAttr("style"); // убираем внутренние стили для иконок
-    },
-    parserOptions: {
-      xmlMode: true
-    },
-  })
-)
-.pipe(replace('&gt;', '>')) // боремся с заменой символа 
-    .pipe(
-      svgSprite({
-        mode: {
-          stack: {
-            sprite: '../sprite.svg', // указываем имя файла спрайта и путь
-          },
-        },
-      })
-    )
-    .pipe(dest('app/images')); // указываем, в какую папку поместить готовый файл спрайта
-}
-
 function images() {
   return src('app/images/**/*.*')
     .pipe(imagemin([
-      imagemin.gifsicle({
-        interlaced: true
-      }),
-      imagemin.mozjpeg({
-        quality: 75,
-        progressive: true
-      }),
-      imagemin.optipng({
-        optimizationLevel: 5
-      }),
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
       imagemin.svgo({
         plugins: [{
             removeViewBox: true
@@ -97,39 +67,76 @@ function images() {
     .pipe(dest('dist/images'))
 }
 
+
 function build() {
   return src([
-      'app/**/*.html',
-      'app/css/style.min.css',
-      'app/js/main.min.js'
-    ], {
-      base: 'app'
+    'app/**/*.html',
+    'app/css/style.min.css',
+    'app/js/main.min.js'
+  ], {
+    base: 'app'
+  })
+  .pipe(dest('dist'))
+}
+
+// function cleanDist() {
+  //   return del('dist')
+  // }
+  
+  
+  function watching() {
+    watch(['app/scss/**/*.scss'], styles);
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+    watch(['app/**/*.html']).on('change', browserSync.reload);
+    watch(['app/images/icons/*.svg'], svgSprites);
+    watch(['app/html/**/*.html'], htmlInclude);
+  }
+  
+  function svgSprites() {
+    return src('app/images/icons/*.svg') // выбираем в папке с иконками все файлы с расширением svg
+    .pipe(cheerio({
+      run: ($) => {
+        $("[fill]").removeAttr("fill"); // очищаем цвет у иконок по умолчанию, чтобы можно было задать свой
+        $("[stroke]").removeAttr("stroke"); // очищаем, если есть лишние атрибуты строк
+        $("[style]").removeAttr("style"); // убираем внутренние стили для иконок
+      },
+      parserOptions: {
+        xmlMode: true
+      },
     })
-    .pipe(dest('dist'))
-}
+  )
+  .pipe(replace('&gt;', '>')) // боремся с заменой символа 
+      .pipe(
+        svgSprite({
+          mode: {
+            stack: {
+              sprite: '../sprite.svg', // указываем имя файла спрайта и путь
+            },
+          },
+        })
+      )
+      .pipe(dest('app/images')); // указываем, в какую папку поместить готовый файл спрайта
+  }
 
-function cleanDist() {
-  return del('dist')
-}
-
-
-
-function watching() {
-  watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/**/*.html']).on('change', browserSync.reload);
-  watch(['app/images/icons/*.svg'], svgSprites);
-}
-
+  const htmlInclude = () => {
+    return src(['app/html/*.html']) // Находит любой .html файл в папке "html", куда будем подключать другие .html файлы													
+      .pipe(fileInclude({
+        prefix: '@',
+        basepath: '@file',
+      }))
+      .pipe(dest('app')) // указываем, в какую папку поместить готовый файл html
+      .pipe(browserSync.stream());
+  }
 
 exports.styles      = styles;
 exports.scripts     = scripts;
 exports.browsersync = browsersync;
-exports.images      = images;
-exports.svgSprites  = svgSprites;
 exports.watching    = watching;
+exports.images      = images;
+// exports.cleanDist   = cleanDist;
 exports.build       = build;
-exports.cleanDist   = cleanDist;
+exports.svgSprites  = svgSprites;
+exports.htmlInclude = htmlInclude;
 
-exports.build       = series(cleanDist, images, build);
-exports.default     = parallel(svgSprites, styles, scripts, browsersync, watching);
+// exports.build       = series(cleanDist, images, build);
+exports.default = parallel(htmlInclude, styles, scripts, browsersync, watching, svgSprites);
